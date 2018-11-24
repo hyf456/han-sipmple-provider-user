707e6fb66a4aec7163fc2678163919d4a0988fdd
hyf456 <hanyf493@163.com>
2018-11-24 20:23:06 +0800

    eureka

--- src/main/java/com/han/cloud/HanSipmpleProviderUserApplication.java
+++ src/main/java/com/han/cloud/HanSipmpleProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.han.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HanSipmpleProviderUserApplication {
 
 	public static void main(String[] args) {
